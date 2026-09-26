87cfad1839fbae3db661550e65dd5fddf0e3bdfc
Szyszka331 <krzychomistrz111@gmail.com>
2026-09-26 12:26:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='time4heroes-3956';
+const CACHE='time4heroes-3957';
 const CORE=[
   './','./index.html','./styles.css?v=3956','./app.js?v=3956','./data.js?v=3956','./manifest.webmanifest',
   './assets/icon-192.png','./assets/icon-512.png','./assets/characters/heroes-directions-391.png',
