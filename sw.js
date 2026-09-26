af58df571d8b59b73066e3b7325e6e17d78e19b0
Szyszka331 <krzychomistrz111@gmail.com>
2026-09-26 12:21:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='time4heroes-3953';
+const CACHE='time4heroes-3956';
 const CORE=[
-  './','./index.html','./styles.css?v=3953','./app.js?v=3953','./data.js?v=3953','./manifest.webmanifest',
+  './','./index.html','./styles.css?v=3956','./app.js?v=3956','./data.js?v=3956','./manifest.webmanifest',
   './assets/icon-192.png','./assets/icon-512.png','./assets/characters/heroes-directions-391.png',
   './assets/characters/battle-backs/knight-back.png','./assets/characters/battle-backs/mage-back.png','./assets/characters/battle-backs/hunter-back.png','./assets/characters/battle-backs/berserker-back.png','./assets/characters/battle-backs/ranger-back.png',
   './assets/tavern-scene-desktop.png','./assets/tavern-scene-mobile.png',
